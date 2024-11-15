861658b4db313138b2ee670a88a5af066eff6f62
Kosmas Nick <n.kosmas89@gmail.com>
2024-11-15 19:30:14 +0200

    Update GcD.py

--- GcD.py
+++ GcD.py
@@ -6,7 +6,7 @@ x = int(input("Enter number for  x: "))  #user will decide the number of x.
 y = int(input("Enter number for  y: "))  #user will decide the number of y.
 while y != 0:                            # we continue repeats as long as  y is not 0.
     z = x % y                            # we calculate the remainder of x and y by dividing them.
-x, y = y, z                              # replace x with the value of y and the value of y with the value of z.(the remainder)
+    x, y = y, z                              # replace x with the value of y and the value of y with the value of z.(the remainder)
                                          
 
 print("The Greatest  Common Divisor  is:", x)
